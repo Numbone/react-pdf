dff10bc185e83e5ea1ddea268f9040eda67f59fb
Nurlybek <nurlybekqwerty@gmail.com>
2024-01-26 13:37:00 +0600

    test

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { PDFViewer } from "@react-pdf/renderer";
+import "./App.css";
+import { MyDocument } from "./components/MyDocument";
+function App() {
+  return (
+    <PDFViewer>
+      <MyDocument />
+    </PDFViewer>
+  );
+}
+
+export default App;
